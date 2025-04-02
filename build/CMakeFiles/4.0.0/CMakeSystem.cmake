61673752b969a3806202ee9d3f9672e29e2c9f59
Buchstabenwurst <jo.okeleke.yahoo.de@gmail.com>
2025-04-02 18:07:47 +0200

    original

---
+++ build/CMakeFiles/4.0.0/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.13.8-arch1-1")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.13.8-arch1-1")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/opt/wonderful/thirdparty/blocksds/core/cmake/BlocksDS.cmake")
+
+set(CMAKE_SYSTEM "NintendoDS-1")
+set(CMAKE_SYSTEM_NAME "NintendoDS")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "armv5te")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
